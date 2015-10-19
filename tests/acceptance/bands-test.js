61976f38104f7092786b34d734a63ae40f81e4ca
starspace1 <lkb@alum.mit.edu>
2015-10-19 14:58:38 -0400

    Adding 2 more tests

--- tests/acceptance/bands-test.js
+++ tests/acceptance/bands-test.js
@@ -21,20 +21,20 @@ test('List bands', function(assert) {
     this.get('/bands', function() {
       var response = {
         data: [
-          {
-            id: 1,
-            type: "bands",
-            attributes: {
-              name: "Radiohead"
-            }
-          },
-          {
-            id: 2,
-            type: "bands",
-            attributes: {
-              name: "Long Distance Calling"
-            }
+        {
+          id: 1,
+          type: "bands",
+          attributes: {
+            name: "Radiohead"
           }
+        },
+        {
+          id: 2,
+          type: "bands",
+          attributes: {
+            name: "Long Distance Calling"
+          }
+        }
         ]
       };
       return [200, { "Content-Type": "application/vnd.api+json" }, JSON.stringify(response)];
@@ -48,4 +48,98 @@ test('List bands', function(assert) {
     assert.equal(find('.band-link:contains("Radiohead")').length, 1, "First band link contains the band name.");
     assert.equal(find('.band-link:contains("Long Distance Calling")').length, 1, "The other band link contains the band name.");
   });
+});
+
+test('Create a new band', function(assert) {
+  server = new Pretender(function() {
+
+    this.get('/bands', function() {
+      var response = {
+        data: [
+        {
+          id: 1,
+          type: "bands",
+          attributes: {
+            name: "Radiohead"
+          }
+        }
+        ]
+      };
+      return [200, { "Content-Type": "application/vnd.api+json" }, JSON.stringify(response)];
+    });
+
+    this.post('/bands', function() {
+      var response = {
+        data: [
+        {
+          id: 2,
+          type: "bands",
+          attributes: {
+            name: "Long Distance Calling"
+          }
+        }
+        ]
+      };
+      return [200, { "Content-Type": "application/vnd.api+json" }, JSON.stringify(response)];
+    });
+
+  });
+
+  visit('/bands');
+  fillIn('.new-band', 'Long Distance Calling'); // Sets value of text box w/ class new-band
+  click('.new-band-button');
+
+  andThen(function() {
+    assert.equal(find('.band-link').length, 2, "All band links are rendered");
+    assert.equal(find('.band-link:last').text().trim(), 'Long Distance Calling', "Created band appears at the end of the list");
+    assert.equal(find('.nav a.active:contains("Songs")').length, 1, "The Songs tab is active");
+  });
+
+});
+
+test('Create a new song in two steps', function(assert) {
+  server = new Pretender(function() {
+
+    this.get('/bands', function() {
+      var response = {
+        data: [
+          {
+            id: 1,
+            type: "bands",
+            attributes: {
+              name: "Radiohead"
+            }
+          }
+        ]
+      };
+      return [200, { "Content-Type": "application/vnd.api+json" }, JSON.stringify(response)];
+    });
+
+    this.post('/songs', function() {
+      var response = {
+        data: [
+          {
+            id: 1,
+            type: "songs",
+            attributes: {
+              name: "Killer Cars"
+            }
+          }
+        ]
+      };
+      return [200, { "Content-Type": "application/vnd.api+json" }, JSON.stringify(response)];
+    });
+
+  });
+
+  visit('/');
+  click('.band-link:contains("Radiohead")');
+  click('a:contains("create one")');
+  fillIn('.new-song', 'Killer Cars');
+  triggerEvent('.new-song-form', 'submit');
+
+  andThen(function() {
+    assert.equal(find('.songs .song:contains("Killer Cars")').length, 1, "Creates the song and displays it in the list");
+  });
+
 });
